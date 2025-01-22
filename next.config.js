01f3ab6d3938eb9e61c3db2690d49c149fd4728e
nvasilev <n.vasilev.fmi@gmail.com>
2025-01-22 14:54:40 +0200

    initial commit

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const { locales } = require('./lib/_locales')
+
+module.exports = {
+  i18n: {
+    defaultLocale: locales.find((locale) => locale.default).value,
+    locales: locales.map((locale) => locale.value)
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.graphassets.com',
+      },
+    ],
+    // Deprecate domain after asset migration
+    domains: ['media.graphassets.com']
+  },
+
+}
