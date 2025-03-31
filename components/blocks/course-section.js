4448dba6a3a9cbde45628973ba850879ffb2e763
Rodrigo Amilivia <r.amilivia@gmail.com>
2025-03-31 12:39:38 +0200

    Responsiveness Course Section

--- components/blocks/course-section.js
+++ components/blocks/course-section.js
@@ -33,7 +33,7 @@ export default function CourseSection({ heading, description, salesforceCourses 
             border="3px solid"
             borderColor="gray.900"
             borderRadius="lg"
-            maxW={{lg: '490px', xl: '580px'}}
+            maxW={{lg: '505px', xl: '580px'}}
             p={4}
           >
             <Heading
