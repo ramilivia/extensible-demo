a82b5b1139fb3cbaceef7203e1e77040cfabaf69
Rodrigo Amilivia <r.amilivia@gmail.com>
2025-05-16 09:51:07 +0200

    fixed alternate in tablet

--- components/sections/alternating-panels-section.js
+++ components/sections/alternating-panels-section.js
@@ -10,14 +10,14 @@ export default function AlternatingPanels({ buttons, image, alternatingPanelsTit
 console.log(alternatingPanelsTitle, 'TITLE')
 
   return (
-    <Box position="relative" bg="gray.50" minH={{ base: 'auto' }} py={{ base: 8, md: 50}}>
+    <Box position="relative" bg="gray.50" minH={{ base: 'auto' }} py={{ base: 8, md: 10, lg: 50 }}>
       <Container maxW="container.xl" h="full">
         <Flex
           direction={{ base: 'column', md: assetPosition === 'right' ? 'row' : 'row-reverse' }}
           align={{ base: 'center', md: 'center' }}
           justify="space-between"
           gap={{ base: 8, md: 12 }}
-          minH={{ base: 'auto', md: '600px' }}
+          minH={{ base: 'auto', md: '420px', lg: '600px' }}
         >
           {/* Content Section */}
           <Stack 
