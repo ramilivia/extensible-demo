583fc8c4c5e3b70b0b91e1cff768ab75fa3e97be
Rodrigo Amilivia <r.amilivia@gmail.com>
2025-06-05 10:10:10 +0200

    Added support for query versions

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -18,12 +18,27 @@ export default async function handler(req, res) {
   const [rootSlug, nestedSlug] = req.query.slug.split('/')
 
 
-  const data = await client.request(pageQuery, {
-    slug: rootSlug,
-    ...(rootSlug && { locale: 'en' })
-  })
+  console.log('HYGRAPH VERSION', process.env.NEXT_PUBLIC_VERSION);
+  const queryFile = await loadQuery(process.env.NEXT_PUBLIC_VERSION);
+  console.log('QUERY FILE', queryFile);
 
-  console.log('DATA', data)
+  let page;
+
+  if (!queryFile) {
+    console.log('NO QUERY FILE');
+    const result = await client.request(pageQuery, {
+      slug: rootSlug,
+      ...(rootSlug && { locale: 'en' })
+    });
+    page = result.page;
+  } else {
+    console.log('QUERY FILE ', process.env.NEXT_PUBLIC_HYGRAPH_VERSION);
+    const result = await client.request(queryFile, {
+      slug: rootSlug,
+      ...(rootSlug && { locale: 'en' })
+    });
+    page = result.page;
+  }
 
   if (!data) {
     return res
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { getPageLayout } from '@/layout'
 import { hygraphClient } from '@/lib/_client'
 import { pageQuery, siteConfigurationQuery } from '@/lib/_queries'
+import { loadQuery } from '@/lib/queryLoader'
 import { parsePageData } from '@/utils/_parsePageData'
 import Wrapper from '@/components/layout/wrapper'
 
@@ -15,13 +16,33 @@ export async function getStaticProps({ locale, preview = false }) {
     brandName: process.env.NEXT_PUBLIC_BRAND_NAME
   })
   console.log('LOCALE', locale);
-  const { page } = await client.request(pageQuery, {
-    locale,
-    slug: 'home'
-  })
+
+  console.log('HYGRAPH VERSION', process.env.NEXT_PUBLIC_VERSION);
+  const queryFile = await loadQuery(process.env.NEXT_PUBLIC_VERSION);
+  console.log('QUERY FILE', queryFile);
+
+  let page;
+
+
+
+  if (!queryFile) {
+    console.log('NO QUERY FILE');
+    const result = await client.request(pageQuery, {
+      locale,
+      slug: 'home'
+    });
+    page = result.page;
+  } else {
+    console.log('QUERY FILE ', process.env.NEXT_PUBLIC_HYGRAPH_VERSION);
+    const result = await client.request(queryFile, {
+      locale,
+      slug: 'home'
+    });
+    page = result.page;
+  }
 
   const parsedPageData = await parsePageData(page)
-  console.log(parsedPageData)
+  
   return {
     props: {
       page: parsedPageData,
