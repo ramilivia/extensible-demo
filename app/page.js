9630ea2243891e6fe8913240078d9bbdbdd26220
Rodrigo Amilivia <r.amilivia@gmail.com>
2025-09-18 22:05:08 +0200

    removed force-dynamic

--- app/page.js
+++ app/page.js
@@ -1,9 +1,6 @@
 import { getHomepageData } from '@/lib/fetchers/page'
 import PageWrapper from './page-wrapper'
 
-// Force dynamic rendering
-export const dynamic = 'force-dynamic'
-
 
 export default async function HomePage() {
   const data = await getHomepageData()
