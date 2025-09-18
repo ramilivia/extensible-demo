dd3e8ad49970f06e112070949404111bf56249bb
Rodrigo Amilivia <r.amilivia@gmail.com>
2025-09-19 00:12:39 +0200

    Removal redundant files

--- components/layout/index.js
+++
@@ -1,6 +0,0 @@
-export {
-  default as ContentLayout,
-  getLayout as getContentLayout
-} from './content'
-export { default as PageLayout, getLayout as getPageLayout } from './page'
-export { default as SiteLayout, getLayout as getSiteLayout } from './site'
