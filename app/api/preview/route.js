0063596ae37c509bf222eeb0765c1d7dab7d4906
Rodrigo Amilivia <r.amilivia@gmail.com>
2025-09-22 19:42:16 +0200

    Fixed preview problem

--- app/api/preview/route.js
+++ app/api/preview/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { draftMode } from 'next/headers'
+import { draftMode, cookies } from 'next/headers'
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url)
@@ -15,9 +15,19 @@ export async function GET(request) {
   ) {
     return NextResponse.json({ message: 'Invalid token' }, { status: 401 })
   }
-
   // Enable Draft Mode immediately after secret validation
   draftMode().enable()
+
+
+  // Known Next.js issue: https://github.com/vercel/next.js/issues/49433
+  const draft = cookies().get('__prerender_bypass')
+
+  cookies().set('__prerender_bypass', draft?.value, {
+    httpOnly: true,
+    sameSite: 'None',
+    secure: true,
+    path: '/',
+  });
 
   const redirectUrl = slug === 'home' ? '/' : `/${slug}`
 
